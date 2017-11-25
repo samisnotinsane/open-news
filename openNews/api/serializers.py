b0d215c4733296f1324073d48dcbc59de9a83546
starfighter-ix <ec14370@qmul.ac.uk>
2017-11-25 14:01:40 +0000

    Adding model and serializer.

---
+++ openNews/api/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from api.models import Section, Article
+
+class SectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Section
+        fields = ('text')
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    post = serializers.HyperlinkedRelatedField(
+        read_only = False,
+        view_name = 'post' 
+    )
+    class Meta:
+        model = Article
+        fields = ('headline', 'like', 'dislike', 'post')
+
