8fc1e8e5c2dde6dff82e7f65f28a252420278dd6
starfighter-ix <ec14370@qmul.ac.uk>
2017-11-25 18:19:09 +0000

    Updating API serializer with create method.

--- openNews/api/serializers.py
+++ openNews/api/serializers.py
@@ -1,11 +1,14 @@
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from mainapp.models import Section, Article
 
 class SectionSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.CharField(read_only=True, allow_null=False)
     class Meta:
         model = Section
         fields = ('name',)
+        read_only_fields = ('name',)
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     post = serializers.HyperlinkedRelatedField(
@@ -15,3 +18,6 @@ class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Article
         fields = ('headline', 'like', 'dislike', 'post',)
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
