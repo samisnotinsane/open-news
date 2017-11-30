0b92da0af1fbe9e1e1faa8c94e59f95c561cc5e1
starfighter-ix <ec14370@qmul.ac.uk>
2017-11-30 17:55:21 +0000

    Writing unit tests for Section

--- openNews/api/tests.py
+++ openNews/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Section
 
 # Create your tests here.
+class SectionTestCase(TestCase):
+    def setUp(self):
+        Section.objects.create(name="TestSection1")
+        Section.objects.create(name="TestSection2")
+    
+    def test_one(self):
+        section1 = Section.objects.get(name="TestSection1")
+        section2 = Section.objects.get(name="TestSection2")
+        self.assertEqual(section1, section2)
+    
+    def test_two(self):
+        section1 = Section.objects.get(name="TestSection1")
+        section2 = Section.objects.get(name="TestSection2")
+        self.assertNotEqual(section1, section2)
