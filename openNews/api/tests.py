d6ebad6b6b97e233285f0db00181eaf3f2a7ccfc
starfighter-ix <ec14370@qmul.ac.uk>
2017-12-05 16:46:41 +0000

    Adding unit tests for article.

--- openNews/api/tests.py
+++ openNews/api/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Section
+from .models import Section, Article
 
 # Create your tests here.
 class SectionTestCase(TestCase):
@@ -10,9 +10,36 @@ class SectionTestCase(TestCase):
     def test_one(self):
         section1 = Section.objects.get(name="TestSection1")
         section2 = Section.objects.get(name="TestSection2")
-        self.assertEqual(section1, section2)
-    
-    def test_two(self):
-        section1 = Section.objects.get(name="TestSection1")
-        section2 = Section.objects.get(name="TestSection2")
         self.assertNotEqual(section1, section2)
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        Article.objects.create(
+            headline="Test Headline 1",
+            body="Body of Test Headline 1"
+            )
+        Article.objects.create(
+            headline="Test Headline 2",
+            body="Body of Test Headline 2"
+            )
+    
+    def test_headline_body_not_equals(self):
+        article = Article.objects.get(headline="Test Headline 1")
+        headline = article.headline
+        body = article.body
+        self.assertNotEqual(headline, body)
+    
+    def test_headlines_not_equals(self):
+        article1 = Article.objects.get(headline="Test Headline 1")
+        article2 = Article.objects.get(headline="Test Headline 2")
+        headline1 = article1.headline
+        headline2 = article2.headline
+        self.assertNotEqual(headline1, headline2)
+
+    def test_bodies_not_equals(self):
+        article1 = Article.objects.get(headline="Test Headline 1")
+        article2 = Article.objects.get(headline="Test Headline 2")
+        body1 = article1.body
+        body2 = article2.body
+        self.assertNotEqual(body1, body2)
+
