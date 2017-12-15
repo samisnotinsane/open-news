223fc7642fa3437df471976e0dfbfc817aec3342
starfighter-ix <ec14370@qmul.ac.uk>
2017-12-15 09:30:08 +0000

    Adding '127.0.0.1' to list of allowed hosts.

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -39,6 +39,7 @@ DEBUG = 'True'
 
 from socket import gethostname
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'localhost',
     gethostname(), # For internal OpenShift load balancer security purposes.
     os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
